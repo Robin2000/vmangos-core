4efeef29adf579c60ccd798d963583304062e31e
29698868 <29698868@qq.com>
2019-02-10 22:30:07 +0800

    部落的示威任务无法放置旗帜的bug

--- sql/mypatch/2019-02-10_quest.sql
+++ sql/mypatch/2019-02-10_quest.sql
@@ -1,3 +1,4 @@
+#https://github.com/cmangos/issues/wiki/dbscripts
 #1657 臭气熏天的南海镇
 #item=20387 亡灵臭气弹
 #item点击施法24706投掷臭气弹
@@ -8,3 +9,10 @@
 #datalong2=1表示允许组队完成任务
 
 insert into event_scripts(id,command,datalong,datalong2,comments) values(9417,8,15415,1,'臭气熏天的南海镇');
+
+
+#3514 部落的示威
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawnFlags`, `visibilitymod`, `patch_min`, `patch_max`)
+VALUES
+   (3998643, 152093, 1, 2371.22, 1796.58, 365.068, 0.3163, 0, 0, 0.157491, 0.98752, 25, 25, 100, 1, 0, 0, 0, 10);

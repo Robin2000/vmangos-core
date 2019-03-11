882295397b5f0ef89087402fb4967991bf04e586
robinxu <29698868@qq.com>
2019-03-11 18:35:45 +0800

    软泥怪变色，待测试

--- sql/mypatch/2019-03-11_npc.sql
+++ sql/mypatch/2019-03-11_npc.sql
@@ -1,2 +1,16 @@
 /*为花丛蛇设置被动法术，增强恐惧，待测试*/
 update creature_template set spell1=6243 where entry=3680;
+
+/*瘟疫软泥（蓝）变形*/
+update creature_template set spell1=28988 where entry=16783;
+
+/*瘟疫软泥（红）变形*/
+update creature_template set spell1=28987 where entry=16784;
+
+/*天灾软泥（绿）变形*/
+update creature_template set spell1=28990 where entry=16785;
+update locales_creature set name_loc4='瘟疫软泥（绿）' where entry=16785;
+
+/*天灾软泥怪（黑）变形*/
+update creature_template set spell1=28989 where entry=16243;
+update locales_creature set name_loc4='瘟疫软泥（黑）' where entry=16243;

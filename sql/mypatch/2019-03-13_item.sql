8aca912df9c60b3891a37b6a473344a64fbe215e
robinxu <29698868@qq.com>
2019-03-13 17:39:32 +0800

    大师雷尔松的全视之眼

--- sql/mypatch/2019-03-13_item.sql
+++ sql/mypatch/2019-03-13_item.sql
@@ -1,2 +1,4 @@
 update locales_item set name_loc4='BKP "冲击" 弹丸' where entry=3034;
 update locales_item set name_loc4='浮空城披风' where entry=23050;
+update locales_item set name_loc4='大师雷尔松的全视之眼' where entry=17354;
+

5612843dec1d3ebe99ea428a6f9b9d15a076da06
robinxu <29698868@qq.com>
2019-03-13 23:01:39 +0800

    ITEM SPELL ERROR

--- sql/mypatch/2019-03-13_item.sql
+++ sql/mypatch/2019-03-13_item.sql
@@ -1,4 +1,5 @@
 update locales_item set name_loc4='BKP "冲击" 弹丸' where entry=3034;
 update locales_item set name_loc4='浮空城披风' where entry=23050;
 update locales_item set name_loc4='大师雷尔松的全视之眼' where entry=17354;
-
+update item_template set name='Tablet of Water Breathing' where entry=9092;
+update locales_item set name_loc4='水下呼吸石板' where entry=9092;

a15e58ce9d7a9c108941c8cdd4da10345f0c9199
robinxu <29698868@qq.com>
2019-03-21 22:05:44 +0800

    重要npc的名字版本恢复

---
+++ sql/mypatch/2019-03-17_Rexxar.sql
@@ -0,0 +1,25 @@
+﻿罗卡鲁to雷克萨
+
+update locales_creature set name_loc4='雷克萨' where entry=10182;
+update locales_item set name_loc4='雷克萨的证明信' where entry=16785;
+
+update locales_quest set title_loc4='雷克萨的证明信' where entry=6568;
+update locales_quest set Objectives_loc4='把雷克萨的信交给西瘟疫之地的巫女麦兰达。' where entry=6568;
+update locales_quest set offerRewardText_loc4='<米兰达拿着便笺看了看，然後笑了笑。>$B$B他最近怎麽样了?我说的是雷克萨。哦，过来，不要感到奇怪。这张信纸是故意留着空白的，其中包含了雷克萨的意图和想法。从其中的内容来看，无疑是雷克萨故意这麽做的。如果你被抓了或者被杀了的话，其中的资讯也不会泄露!$B$B哦，不要介意!米兰达会帮你的，$r。我欠酋长的情。$B$B<米兰达微笑着。>$B' where entry=6568;
+
+update locales_quest set Objectives_loc4='看来这场假面舞会就要结束了。你知道麦兰达为你制作的龙形护符在黑石塔里面不会发挥作用，也许你应该去找雷克萨，将你的困境告诉他。把黯淡的龙火护符给他看看，也许他知道下一步该怎么做。' where entry=6601;
+
+update locales_quest set Objectives_loc4='到黑石塔去杀掉达基萨斯将军，把它的血交给雷克萨。' where entry=6602;
+
+update locales_quest set Objectives_loc4='到黑石塔去杀掉达基萨斯将军，把它的血交给雷克萨。' where entry=6567;
+
+update locales_broadcast_text set MaleText_loc4='你好，$c。我是雷克萨。' where id=9013;
+update locales_broadcast_text set MaleText_loc4='雷克萨，我丢了龙火护符。你能再给我一个吗？' where id=9014;
+update locales_broadcast_text set FemaleText_loc4='雷克萨，我丢了龙火护符。你能再给我一个吗？' where id=9014;
+
+
+update locales_gossip_menu_option set option_text_loc4='雷克萨，我把龙火护符弄丢了。可以再创造一个吗?' where menu_id=10182 and id=0;
+
+
+
+
---
+++ sql/mypatch/2019-03-21_npc.sql
@@ -0,0 +1,23 @@
+﻿赫米特·奈辛瓦里二世 to 赫米特·奈辛瓦里
+
+update locales_creature set name_loc4='黑色其拉作战坦克' where entry=15666 and name_loc4='蓝色其拉作战坦克';
+update locales_creature set name_loc4='食尸鬼' where entry=846 and name_loc4='腐化的食尸鬼';
+
+update locales_creature set name_loc4='赫米特·奈辛瓦里' where entry=715 and name_loc4='赫米特·奈辛瓦里二世';
+
+update locales_quest set Objectives_loc4='赫米特·奈辛瓦里要求你杀死10只荆棘谷迅猛龙。',
+RequestItemsText_loc4='我从没见过猎人站在营火旁边也能打到猎物的。你是打算杀死荆棘谷迅猛龙还是等那些猎物老死?' where entry=194;
+
+update locales_quest set Objectives_loc4='赫米特·奈辛瓦里要求你杀死10只鞭尾迅猛龙。' where entry=195;
+update locales_quest set Objectives_loc4='赫米特·奈辛瓦里要求你杀死10只丛林捕猎者' where entry=196;
+update locales_quest set Objectives_loc4='赫米特·奈辛瓦里要求你杀死荆棘谷中最危险最狡猾的迅猛龙泰希斯。' where entry=197;
+update locales_quest set Objectives_loc4='赫米特·奈辛瓦里要求你将虎王邦加拉什的头带回来给他。' where entry=208;
+update locales_quest set Objectives_loc4='与赫米特·奈辛瓦里谈一谈。',
+Details_loc4='欢迎来到荆棘谷！$B$B也许你并没有意识到这一点，但那个矮人是赫米特·奈辛瓦里，着名的联盟战争英雄和大师级猎人。 他并不是一个欢迎陌生人进入他的阵营的人，但是你看起来他已经留意你有些日子了，$C.$B$B作为战斗老将，奈辛瓦里大师有一个弱点，对同胞英雄情有独钟。$B$B去和他说话。 也许他可以给你一些狩猎指点。'
+where entry=583;
+update locales_quest set Objectives_loc4='将鲁恩的科多兽角送到荆棘谷的赫米特·奈辛瓦里那里。',
+Details_loc4='很久以前，一个矮人来到这片土地。 他的名字是赫米特，他希望捕杀伟大的野兽。 他用步枪的技巧是不可思议的，我们花了很多天一起打猎。 甚至我们两国人民之间的敌意也被遗忘了。$B$B当他离开凄凉之城去荆棘谷时，我发誓有一天欢迎他回来让我们一起打猎。 现在就是那个时候。$B$B将这个科多兽的号角转向赫米特。 他会知道这是来自我的。 你会发现他在格罗姆高营地以北的荆棘谷。'
+where entry=5763;
+update locales_quest set Objectives_loc4='将克拉维尔的箱子送到荆棘谷的赫米特·奈辛瓦里那里。',
+Details_loc4='我为我的一位老客户提供了一个包裹，一个名叫赫米特·奈辛瓦里的矮人。 包裹花了几周才到达，赫米特已经过去了很长时间。 他说他要去荆棘谷捕猎那里的野兽，但是他给我留了一些钱，可以随时送他送货。$B$B赫米特是一个富有的矮人，与富人保持联系是一个好主意 是的，是吗？ 那么......你想为我送包裹吗？$B$B我听说赫米特在格罗姆北部的荆棘谷有一个营地。'
+where entry=5762;
